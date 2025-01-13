787adbf22ade10da16c1910b15fa962aaa50af21
shaxzodabdualimov <shaxzodabdualimov2009@gmail.com>
2025-01-13 19:52:46 +0500

    ARRAYLIST
    ARRAYLIST

---
+++ ArrayAndLinkedList/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Objects;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+//
+//        LinkedList<String> toplam2 = new LinkedList<>();
+//        toplam2.add("Tom");
+//        toplam2.add("Jack");
+//        toplam2.add("James");
+//        toplam2.add("John");
+//        toplam2.add("Jane1");
+//        System.out.println(toplam2);
+//        toplam2.pop();
+//        System.out.println(
+//                toplam2
+//        );
+//        toplam2.push("Shimara");
+//
+//        System.out.println(toplam2);
+
+
+
+    }
+}
---
+++ ArrayAndLinkedList/src/Person.java
@@ -0,0 +1,6 @@
+public class Person {
+    String name;
+    String surname;
+    int age;
+
+}
